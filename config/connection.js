d6d3458ea1dab60a975651dd870779448be14e67
Michael Olshewski <michael.olshewski@yahoo.com>
2020-08-24 19:40:08 -0400

    removed space

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: '	qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host: 'qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
     user: 'havu4sprheh8nc7c',
     password: 'ecu68q3lstyn6xja',
