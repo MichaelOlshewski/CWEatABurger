0a7c1f54c08eb9e3ab62fa22a80da8e3a02bddaf
Michael Olshewski <michael.olshewski@yahoo.com>
2020-08-26 17:40:39 -0400

    Updated schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+CREATE DATABASE burger_db;
+USE burger_db;
 
-CREATE TABLE cats
+CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	isDevoured BOOLEAN DEFAULT false
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name, isDevoured) VALUES ('Chicken Sandwich', false);
+INSERT INTO burgers (burger_name, isDevoured) VALUES ('Big Buford', false);
+INSERT INTO burgers (burger_name, isDevoured) VALUES ('Baconator', true);
+INSERT INTO burgers (burger_name, isDevoured) VALUES ('Quarter Pounder w/ Cheese', true);
