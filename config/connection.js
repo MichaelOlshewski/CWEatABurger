0e346fae8ba42a972d1b3851d474af9f798b3f85
Michael Olshewski <michael.olshewski@yahoo.com>
2020-08-26 15:43:29 -0400

    Updated Styling

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,20 @@
 var mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: 'qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  port: 3306,
-  user: 'havu4sprheh8nc7c',
-  password: 'ecu68q3lstyn6xja',
-  database: 'kclx6u90ypoqbzw0'
+    host: 'qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'havu4sprheh8nc7c',
+    password: 'ecu68q3lstyn6xja',
+    database: 'kclx6u90ypoqbzw0'
 })
 
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
