fd7d823f7cf66d38c734c310b46f2c9883fc52a7
Michael Olshewski <michael.olshewski@yahoo.com>
2020-08-27 11:33:46 -0400

    Added ENV variables

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: process.env.PORT || 3306,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
+// var connection = mysql.createConnection({
+//     host: process.env.HOST,
+//     port: process.env.PORT || 3306,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE
+// });
+
+var connection
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: process.env.HOST,
+        port: process.env.PORT || 3306,
+        user: process.env.USER,
+        password: process.env.PASS,
+        database: process.env.DATABASE
+    })
+}
 
 // Make connection.
 connection.connect(function(err) {
