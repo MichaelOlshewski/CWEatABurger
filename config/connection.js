5ac76c81f5e1149a0c088bf06ed95c4b57d5fc40
Michael Olshewski <michael.olshewski@yahoo.com>
2020-08-26 21:45:41 -0400

    Fixed DB Error

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    host: "qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
     user: "havu4sprheh8nc7c",
     password: "ecu68q3lstyn6xja",
